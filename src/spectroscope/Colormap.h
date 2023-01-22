d8977765640a276bc53d388c81769645a364ff66
Jürgen Hock <juergen.hock@jurihock.de>
2023-01-22 18:39:44 +0100

    Add gamma correction

--- src/spectroscope/Colormap.h
+++ src/spectroscope/Colormap.h
@@ -4,8 +4,8 @@
 #include <opencv2/imgproc.hpp>
 
 #include <algorithm>
+#include <cmath>
 #include <numeric>
-#include <span>
 #include <vector>
 
 class Colormap
@@ -17,7 +17,8 @@ public:
     min(min),
     max(max),
     slope(+1.0 / (max - min)),
-    intercept(-min / (max - min))
+    intercept(-min / (max - min)),
+    gamma(1.5)
   {
     std::vector<uint8_t> input(256);
     std::vector<uint8_t> output(256 * 3);
@@ -27,7 +28,7 @@ public:
     cv::Mat gray(1, 256, CV_8UC1, input.data());
     cv::Mat color(1, 256, CV_8UC3, output.data());
 
-    cv::applyColorMap(gray, color, cv::COLORMAP_MAGMA);
+    cv::applyColorMap(gray, color, cv::COLORMAP_OCEAN);
 
     for (size_t i = 0; i < 256; ++i)
     {
@@ -39,20 +40,17 @@ public:
 
   void tobgr(const double value, uint8_t& b, uint8_t& g, uint8_t& r) const
   {
-    const uint8_t i = static_cast<uint8_t>(
-      0xFF * std::clamp(value * slope + intercept, 0.0, 1.0));
+    double i = value;
 
-    b = lut[i][0];
-    g = lut[i][1];
-    r = lut[i][2];
-  }
+    i = i * slope + intercept;
+    i = std::clamp(i, 0.0, 1.0);
+    i = std::pow(i, gamma);
 
-  void tobgr(const std::span<double> values, const std::span<uint8_t> bgr) const
-  {
-    for (size_t i = 0, j = 0; i < values.size(); ++i, j+=3)
-    {
-      tobgr(values[i], bgr[j + 0], bgr[j + 1], bgr[j + 2]);
-    }
+    uint8_t j = static_cast<uint8_t>(0xFF * i);
+
+    b = lut[j][0];
+    g = lut[j][1];
+    r = lut[j][2];
   }
 
 private:
@@ -61,6 +59,7 @@ private:
   const double max;
   const double slope;
   const double intercept;
+  const double gamma;
 
   uint8_t lut[256][3];
 
