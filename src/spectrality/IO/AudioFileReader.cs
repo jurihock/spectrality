d205d2f07084474f7555d232dcc89261acd48650
Jürgen Hock <juergen.hock@jurihock.de>
2024-04-29 21:57:54 +0200

    Read requested audio channel

--- src/spectrality/IO/AudioFileReader.cs
+++ src/spectrality/IO/AudioFileReader.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace Spectrality.IO;
 
@@ -18,17 +19,36 @@ public class AudioFileReader
     Logger.Info($"Reading audio file \"{Path}\".");
 
     var reader = new NAudio.Wave.AudioFileReader(Path);
+
     var format = reader.WaveFormat;
-
-    var length = reader.Length / (format.BitsPerSample / 8);
-    var samples = new float[length];
-
-    reader.Read(samples, 0, samples.Length);
-
     var samplerate = (double)format.SampleRate;
-    var duration = TimeSpan.FromSeconds(samples.Length / samplerate);
+    var channels = format.Channels;
+    var length = (int)(reader.Length / (format.BitsPerSample / 8));
 
-    Logger.Info($"Read {samples.Length} samples of {duration.TotalSeconds:F3}s duration at {samplerate}Hz.");
+    var samples = new float[length];
+    var result = reader.Read(samples, 0, length);
+
+    Debug.Assert(result == length);
+
+    if (channels > 1)
+    {
+      channel = (channel < 0) ? channels + channel : channel;
+      channel = Math.Clamp(channel, 0, channels - 1);
+
+      length /= channels;
+
+      for (int i = 0, j = channel; i < length; i++, j+=channels)
+      {
+        samples[i] = samples[j];
+      }
+
+      Array.Resize(ref samples, length);
+    }
+
+    Debug.Assert(samples.Length == length);
+
+    var duration = TimeSpan.FromSeconds(length / samplerate);
+    Logger.Info($"Read {length} samples of {duration.TotalSeconds:F3}s duration at {samplerate}Hz.");
 
     return (samples, samplerate);
   }
