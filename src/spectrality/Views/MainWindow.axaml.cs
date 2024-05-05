770e43970b5b90b47be66d189cc48fcfb760a508
Jürgen Hock <juergen.hock@jurihock.de>
2024-05-05 13:55:02 +0200

    Set plot tracker style globally

--- src/spectrality/Views/MainWindow.axaml.cs
+++ src/spectrality/Views/MainWindow.axaml.cs
@@ -4,8 +4,8 @@ namespace Spectrality.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+  public MainWindow()
+  {
+    InitializeComponent();
+  }
 }
