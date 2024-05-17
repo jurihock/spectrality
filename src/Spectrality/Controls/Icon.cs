b9fad07dd5e514d74f120d0e83de912af80f7fda
Jürgen Hock <juergen.hock@jurihock.de>
2024-05-17 23:24:25 +0200

    Set StyleKeyOverride

--- src/Spectrality/Controls/Icon.cs
+++ src/Spectrality/Controls/Icon.cs
@@ -1,9 +1,12 @@
+using System;
 using FluentIcons.Avalonia;
 
 namespace Spectrality.Controls;
 
 public sealed class Icon : SymbolIcon
 {
+  protected override Type StyleKeyOverride => typeof(SymbolIcon);
+
   public Icon()
   {
     IsFilled = true;
