6e15b7a5ff7c187cfce5472b1013a38b610a0a89
Jürgen Hock <juergen.hock@jurihock.de>
2024-05-17 23:08:00 +0200

    Use regular dictionary

--- src/Spectrality/Extensions/LoggingExtensions.cs
+++ src/Spectrality/Extensions/LoggingExtensions.cs
@@ -39,16 +39,15 @@ public static class LoggingExtensions
 
   private sealed class AvaloniaNLogSink : Avalonia.Logging.ILogSink
   {
-    private static readonly IReadOnlyDictionary<Avalonia.Logging.LogEventLevel, NLog.LogLevel> LogLevelMap =
-      new Dictionary<Avalonia.Logging.LogEventLevel, NLog.LogLevel>
-      {
-        { Avalonia.Logging.LogEventLevel.Verbose, NLog.LogLevel.Trace },
-        { Avalonia.Logging.LogEventLevel.Debug, NLog.LogLevel.Debug },
-        { Avalonia.Logging.LogEventLevel.Information, NLog.LogLevel.Info },
-        { Avalonia.Logging.LogEventLevel.Warning, NLog.LogLevel.Warn },
-        { Avalonia.Logging.LogEventLevel.Error, NLog.LogLevel.Error },
-        { Avalonia.Logging.LogEventLevel.Fatal, NLog.LogLevel.Fatal }
-      };
+    private static readonly Dictionary<Avalonia.Logging.LogEventLevel, NLog.LogLevel> LogLevelMap = new()
+    {
+      { Avalonia.Logging.LogEventLevel.Verbose, NLog.LogLevel.Trace },
+      { Avalonia.Logging.LogEventLevel.Debug, NLog.LogLevel.Debug },
+      { Avalonia.Logging.LogEventLevel.Information, NLog.LogLevel.Info },
+      { Avalonia.Logging.LogEventLevel.Warning, NLog.LogLevel.Warn },
+      { Avalonia.Logging.LogEventLevel.Error, NLog.LogLevel.Error },
+      { Avalonia.Logging.LogEventLevel.Fatal, NLog.LogLevel.Fatal }
+    };
 
     private static bool OxyPlotTrackerWarning = false;
 
